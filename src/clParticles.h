57a1718d0cc15a8a9758e1a115e6d670801e4375
greg <gregoire.lejay@gmail.com>
2014-02-09 12:44:00 +0000

    initialising centered particles

--- src/clParticles.h
+++ src/clParticles.h
@@ -5,21 +5,38 @@
 #include "MSAOpenCL.h"
 //-----------------
 
+//------------------------------------------------------------------------------
+typedef struct{
+	float2 vel;
+	float mass;
+	float dummy;
+    float theta;
+    float vTheta;
+    float u;
+    float vU;
+} Particle;
+
+//------------------------------------------------------------------------------
 class clParticles : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+private:
+	void setupParticles();
+	void setupPosition(int i);
 };
+
+//------------------------------------------------------------------------------
